17f02baa7df5e7baad6d8a673a380cb669f1418b
Jarrod Medrano <jmedran@gmail.com>
2019-04-25 10:30:49 -0600

    adding landing page

---
+++ page-landing-sales.php
@@ -0,0 +1,40 @@
+<?php
+	/*
+		Template Name: Sales Landing
+    */
+    ?>
+<!DOCTYPE html>
+<!--[if lt IE 7]>
+<html class="no-js lt-ie9 lt-ie8 lt-ie7" <?php language_attributes(); ?>> <![endif]-->
+<!--[if IE 7]>
+<html class="no-js lt-ie9 lt-ie8" <?php language_attributes(); ?>> <![endif]-->
+<!--[if IE 8]>
+<html class="no-js lt-ie9" <?php language_attributes(); ?>> <![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js" <?php language_attributes(); ?>>
+<!--<![endif]-->
+
+<head>
+    <?php
+	
+   include('/inc/header-scripts.php');
+   wp_head(); 
+    
+    ?>
+
+
+</head>
+
+<body <?php body_class(); ?>>
+
+    <?php wp_footer(); ?>
+    <?php
+/**
+ *
+ * DO NOT WRITE JAVASCRIPT HERE
+ *
+ */
+?>
+</body>
+
+</html>
